d184a327a73c926b8ac6728c4b7abd7e65c29370
JeanPonomarev <jean1995sib@gmail.com>
2020-04-11 20:34:52 +0300

    Добавлена логика проверки страницы с авторизацией/регистрацией

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -15,10 +17,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_should_see_right_login_page(browser):
-    link = Links.LOGIN_PAGE
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
