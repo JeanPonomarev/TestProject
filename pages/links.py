b1fe7548f67e96197f4fd12d17213eba47de1bf4
JeanPonomarev <jean1995sib@gmail.com>
2020-04-14 10:31:17 +0300

    Методы по проеверке перехода на страницу авторизации вынесены в BasePage. Переписано два тест-кейса для проверки перехода на страницу с авторизацией

--- pages/links.py
+++ pages/links.py
@@ -2,3 +2,4 @@ class Links:
     MAIN_PAGE = "http://selenium1py.pythonanywhere.com/"
     LOGIN_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     SHELLCODERS_HANDBOOK_PAGE = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    THE_CITY_AND_THE_STARS_95_PAGE = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
--- test_product_page.py
+++ test_product_page.py
@@ -2,31 +2,46 @@ import pytest
 import time
 
 from .pages.product_page import ProductPage
+from .pages.product_page import Links
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                      marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   pytest.param(
+#                                       "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                       marks=pytest.mark.xfail),
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+# def test_guest_can_add_product_to_basket(browser, link):
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#
+#     initial_product_name = product_page.get_initial_product_name()
+#     initial_product_price = product_page.get_initial_product_price()
+#
+#     product_page.add_product_to_basket()
+#     product_page.solve_quiz_and_get_code()
+#
+#     time.sleep(240)
+#
+#     product_page.should_be_correct_product_name(initial_product_name)
+#     product_page.should_be_correct_price(initial_product_price)
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = Links.THE_CITY_AND_THE_STARS_95_PAGE
     product_page = ProductPage(browser, link)
     product_page.open()
+    product_page.should_be_login_link()
 
-    initial_product_name = product_page.get_initial_product_name()
-    initial_product_price = product_page.get_initial_product_price()
 
-    product_page.add_product_to_basket()
-    product_page.solve_quiz_and_get_code()
-
-    time.sleep(240)
-
-    product_page.should_be_correct_product_name(initial_product_name)
-    product_page.should_be_correct_price(initial_product_price)
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = Links.THE_CITY_AND_THE_STARS_95_PAGE
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_login_page()
